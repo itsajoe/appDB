7888a4128a611fd7364ec5bd909b975eb66bff71
itsajoe <jmusser19@gmail.com>
2018-02-22 11:22:51 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+// require packages
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+// require models
+var db = require('./models');
+// instantiate app
+var PORT = process.env.PORT || 9000;
+var app = express();
+// configure middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+// connect to db
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/appDB";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+// require routes
+
+// listen
+app.listen(PORT, function() {
+    console.log("App is listening on port: " + PORT);
+});
